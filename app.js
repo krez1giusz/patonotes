c0a876957af3003feb137347892f7401de174080
krez1giusz <115666587+krez1giusz@users.noreply.github.com>
2024-02-05 00:19:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,199 @@
+//generateQuestion();
+var questionsAmount = 1;
+let askedQuestions = new Set();
+function generateQuestion() {
+    // let element = questions[1]
+    // console.log(element.pytanie+ " PYTANIE")
+    // console.log(questions.length+ " DLUGOSC TABLICY Z PYTANIAMI")
+    // console.log(usedQuestions.length+ " DRUGA TABLICA")
+
+    
+    // console.log(usedQuestions[element] + " BEFORE")
+    // usedQuestions[element] = true;
+    // console.log(usedQuestions[element] + " AFTER")
+
+    if (questions.length === askedQuestions.size) {
+        console.log("All questions have been asked.");
+        return;
+    }
+
+    let question;
+    do {
+        question = questions[Math.floor(Math.random() * questions.length)];
+    } while (askedQuestions.has(question));
+
+    askedQuestions.add(question);
+    return question;
+}
+
+function startExam(){
+    askedQuestions.clear()
+    clearObjects()
+    const mainBox = document.createElement("div");
+    mainBox.className = "mainBox";
+    const checkButton = document.createElement("button");
+        checkButton.className = 'controlBtn',
+        checkButton.onclick = checkQuestions,
+        checkButton.innerText = "SPRAWDŹ TEST"
+        mainBox.appendChild(checkButton)
+
+    questionsAmount  = document.querySelector("#questionamount").value;
+    
+
+    console.log(questionsAmount,questions.length)
+
+    if(questionsAmount > questions.length){
+        questionsAmount = questions.length
+    }
+    
+
+    for(var i = 0; i < questionsAmount; i++){
+
+        const generatedQuestion = generateQuestion();
+
+        const singleQuestion = document.createElement("div");
+        singleQuestion.className = "singleQuestion";
+
+        const questionSpan = document.createElement("span");
+        questionSpan.className = "question";
+        questionSpan.innerText = generatedQuestion.pytanie;
+        singleQuestion.appendChild(questionSpan);
+  
+        const answersContainer = document.createElement("div");
+        answersContainer.className = "questions";
+    
+        for (const odpowiedz of generatedQuestion.answers) {
+            const answer = createAnswer(odpowiedz.text, odpowiedz.correct)
+            answersContainer.appendChild(answer)
+        }
+
+        singleQuestion.appendChild(answersContainer);
+  
+        mainBox.appendChild(singleQuestion);
+
+
+    
+    }
+
+
+    document.body.appendChild(mainBox);
+}
+
+
+function clearObjects(){
+
+    const mainBox = document.querySelector('.mainBox')
+    if(mainBox == null){
+        return
+    }
+    mainBox.remove();
+    const singleQuestion = document.querySelector('.singleQuestion')
+    if(singleQuestion == null){
+        return
+    }
+    singleQuestion.remove();
+    const question = document.querySelector('.question')
+    if(question == null){
+        return
+    }
+    question.remove();
+    const questions = document.querySelector('.questions')
+    if(questions == null){
+        return
+    }
+    questions.remove();
+    const answer = document.querySelector('.odpowiedz')
+    if(answer == null){
+        return
+    }
+    answer.remove();
+    const inputs = document.querySelectorAll('input')
+    if(inputs == null){
+        return
+    }
+    inputs.forEach(function (input){
+        input.remove();
+    })
+}
+
+
+function createAnswer(text, isCorrect) {
+    const answerDiv = document.createElement("div");
+    answerDiv.className = "odpowiedz";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.dataset.correct = isCorrect;
+
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    answerDiv.appendChild(checkbox);
+    answerDiv.appendChild(span);
+
+    return answerDiv;
+}
+
+function checkQuestions(){
+    let correctAnswers = 0;
+
+    var answers = document.querySelectorAll(".odpowiedz");
+
+    answers.forEach(function(answer){
+        const input = answer.querySelector('input');
+        const span = answer.querySelector('span')
+        console.log(input.dataset.correct,input.checked)
+        if(input.checked){
+            if(input.dataset.correct == "1"){
+                correctAnswers++
+                span.style.color = "green";
+            }else{
+                span.style.color = "red";
+                
+            }
+        }else{
+            if(input.dataset.correct == "1"){
+                span.style.color = "purple"
+            }
+        }
+    })
+
+    alert("WYNIK: "+correctAnswers)
+
+    // var elements = document.querySelectorAll(".odpowiedz");
+
+
+
+    
+    // let correctAnswers = 0;
+    // for( var i = 0; elements[i]; ++i){
+    //     const input = elements[i].querySelector("input");
+    //     const span = elements[i].querySelector("span");
+    //     console.log(input.checked, input.dataset.correct);
+
+    //     if(input.checked && input.dataset.correct){
+    //         correctAnswers++;
+    //         span.style.color = "green";
+    //     }
+
+
+    //     // console.log(elements[i].querySelector('input').checked);
+    //     // if(elements[i].checked) {
+    //     //     if(elements[i].querySelector('input').correct){
+    //     //         correctAnswers++;
+    //     //         elements[i].querySelector('span').style.color = "green";
+    //     //     }else{
+    //     //         // elements[i].
+    //     //     }
+    //     // }else{
+    //     //     if(elements[i].querySelector('input').correct){
+    //     //         elements[i].querySelector('input').checked;
+    //     //     }
+    //     // }
+
+    // }
+    // //alert("POPRAWNE ODPOWIEDZI: "+correctAnswers);
+
+}
+
+
---
+++ pytania.js
@@ -0,0 +1,307 @@
+const questions = [
+    {
+        "pytanie": "Marcin zrobił fikołka, ile zębów stracił?",
+        "alreadyGenerated": false,
+        "answers": [
+            {
+                "text": "A) Cztery zęby",
+                "correct": 1
+            },
+            {
+                "text": "B) Trzy zęby",
+                "correct": 0
+            },
+            {
+                "text": "C) Jeden ząb",
+                "correct": 0
+            },
+            {
+                "text": "D) Dwa zęby",
+                "correct": 0
+            }
+        ]
+    },
+    {
+        "pytanie": "Czy Wieś Żuków, leżąca nieopodal Gminy Sochaczew posiada dostęp do ciepłej wody i prądu?",
+        "alreadyGenerated": false,
+        "answers": [
+            {
+                "text": "A) Tak",
+                "correct": 1
+            },
+            {
+                "text": "B) Nie",
+                "correct": 0
+            },
+            {
+                "text": "C) To zbyt skomplikowane",
+                "correct": 0
+            },
+        ]
+    },
+    {
+        "pytanie": "Jak nazywa się największy pies na świecie?",
+        "alreadyGenerated": false,
+        "answers": [
+            {
+                "text": "A) Clifford",
+                "correct": 0
+            },
+            {
+                "text": "B) Beethoven",
+                "correct": 0
+            },
+            {
+                "text": "C) Fido",
+                "correct": 0
+            },
+            {
+                "text": "D) Bigos",
+                "correct": 1
+            }
+        ]
+    },
+{
+    "pytanie": "Co zrobił Janek, gdy dowiedział się, że jego żona go zdradza?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Zabił ją",
+            "correct": 0
+        },
+        {
+            "text": "B) Zdradził ją z jej siostrą",
+            "correct": 0
+        },
+        {
+            "text": "C) Zmienił hasło do Netflix",
+            "correct": 1
+        },
+        {
+            "text": "D) Nic nie zrobił",
+            "correct": 0
+        }
+    ]
+},
+{
+    "pytanie": "Czy można jeździć na nartach w Warszawie?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, na Kopcu Powstania Warszawskiego",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo nie ma tam śniegu",
+            "correct": 0
+        },
+        {
+            "text": "C) Tak, na lodowisku Torwar",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo to niebezpieczne",
+            "correct": 1
+        }
+    ]
+},
+{
+    "pytanie": "Kto jest autorem słynnej powieści „Lalka”?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Bolesław Prus",
+            "correct": 1
+        },
+        {
+            "text": "B) Henryk Sienkiewicz",
+            "correct": 0
+        },
+        {
+            "text": "C) Adam Mickiewicz",
+            "correct": 0
+        },
+        {
+            "text": "D) Barbie",
+            "correct": 0
+        }
+    ]
+},
+{
+    "pytanie": "Czy można zjeść żabę?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, ale tylko nogi",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo to obrzydliwe",
+            "correct": 0
+        },
+        {
+            "text": "C) Tak, ale tylko żywą",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo to zwierzę chronione",
+            "correct": 1
+        }
+    ]
+},
+{
+    "pytanie": "Co oznacza skrót LOL?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Laughing Out Loud",
+            "correct": 1
+        },
+        {
+            "text": "B) League Of Legends",
+            "correct": 0
+        },
+        {
+            "text": "C) Lots Of Love",
+            "correct": 0
+        },
+        {
+            "text": "D) Lekko Obrzydliwy Lubczyk",
+            "correct": 0
+        }
+    ]
+},
+{
+    "pytanie": "Jak nazywa się najmniejsza planeta w Układzie Słonecznym?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Merkury",
+            "correct": 1
+        },
+        {
+            "text": "B) Pluton",
+            "correct": 0
+        },
+        {
+            "text": "C) Ziemia",
+            "correct": 0
+        },
+        {
+            "text": "D) Saturn",
+            "correct": 0
+        }
+    ]
+},
+{
+    "pytanie": "Czy można pić wodę z kranu w Polsce?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, ale tylko po przegotowaniu",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo jest skażona",
+            "correct": 0
+        },
+        {
+            "text": "C) Tak, ale tylko w niektórych miastach",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo to niezdrowe",
+            "correct": 1
+        }
+    ]
+},
+{
+    "pytanie": "Jak nazywa się najwyższa góra w Polsce?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Rysy",
+            "correct": 1
+        },
+        {
+            "text": "B) Śnieżka",
+            "correct": 0
+        },
+        {
+            "text": "C) Babia Góra",
+            "correct": 0
+        },
+        {
+            "text": "D) Kasprowy Wierch",
+            "correct": 0
+        }
+    ]
+},
+{
+    "pytanie": "Czy można jeździć na rowerze bez kasku?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, ale tylko na chodniku",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo to nielegalne",
+            "correct": 0
+        },
+        {
+            "text": "C) Tak, ale tylko w dzień",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo to niebezpieczne",
+            "correct": 1
+        }
+    ]
+},
+{
+    "pytanie": "Czy można zrobić tort z ogórków?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, ale tylko z kiszonych",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo to niemożliwe",
+            "correct": 0
+        },
+        {
+            "text": "C) Tak, ale tylko z cukrem",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo to obrzydliwe",
+            "correct": 1
+        }
+    ]
+},
+{
+    "pytanie": "Czy można pływać w Wiśle?",
+    "alreadyGenerated": false,
+    "answers": [
+        {
+            "text": "A) Tak, ale tylko w kajaku",
+            "correct": 0
+        },
+        {
+            "text": "B) Nie, bo jest zanieczyszczona",
+            "correct": 1
+        },
+        {
+            "text": "C) Tak, ale tylko w zimie",
+            "correct": 0
+        },
+        {
+            "text": "D) Nie, bo jest za głęboka",
+            "correct": 0
+        }
+    ]
+},
+
+]
