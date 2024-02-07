adb7d249952a95f7399f75f172d2e57297b66dd6
budynn0 <135149640+budynn0@users.noreply.github.com>
2024-02-08 00:46:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -197,7 +197,7 @@ function checkQuestions(){
 
     })
 
-    alert("LICZBA POPRAWNYCH ODPOWIEDZI: "+totalCorrectAnswers+"/"+questionsAmount+"("+(totalCorrectAnswers*100/questionsAmount)+"%)")
+    alert("LICZBA POPRAWNYCH ODPOWIEDZI: "+totalCorrectAnswers+"/"+questionsAmount+"("+((totalCorrectAnswers*100/questionsAmount).toFixed(1))+"%)");
 }
 
 
