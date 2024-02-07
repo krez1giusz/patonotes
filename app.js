addab6f5fd39a0a088a88d951fdb7409a7e8441a
budynn0 <135149640+budynn0@users.noreply.github.com>
2024-02-08 00:39:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var questionsAmount = 1;
 let askedQuestions = new Set();
 var selectedSubject;
@@ -8,17 +9,19 @@ function selectSubject(subject){
 
 }
 
-   function generateQuestion() {
+
+
+
+function generateQuestion() {
 
     if (questions.length === askedQuestions.size) {
-@@ -18,6 +29,12 @@ function generateQuestion() {
-}
-
+        console.log("All questions have been asked.");
+        return;
+    }
     let question;
     do {
         question = questions[Math.floor(Math.random() * questions.length)];
     } while (askedQuestions.has(question));
-
     askedQuestions.add(question);
     return question;
 }
@@ -33,15 +36,18 @@ function startExam(){
     askedQuestions.clear()
     clearObjects()
     const mainBox = document.createElement("div");
+    mainBox.className = "mainBox";
+    questionsAmount  = document.querySelector("#questionamount").value;
     
-
+    console.log(questionsAmount,questions.length)
+    if(questionsAmount > questions.length){
+        questionsAmount = questions.length
+    }
+    
     for(var i = 0; i < questionsAmount; i++){
-
         const generatedQuestion = generateQuestion();
-
         const singleQuestion = document.createElement("div");
         singleQuestion.className = "singleQuestion";
-
         const questionSpan = document.createElement("span");
         questionSpan.className = "question";
         questionSpan.innerText = generatedQuestion.pytanie;
@@ -49,34 +55,23 @@ function startExam(){
   
         const answersContainer = document.createElement("div");
         answersContainer.className = "questions";
-
         if(generatedQuestion.imgPath != undefined){
             const image = document.createElement('img')
             image.src = generatedQuestion.imgPath;
             singleQuestion.appendChild(image)
         }
-
-
         
-
         for (const odpowiedz of shuffleArray(generatedQuestion.answers)) {
             const answer = createAnswer(odpowiedz.text, odpowiedz.correct)
             answersContainer.appendChild(answer)
         }
-
         singleQuestion.appendChild(answersContainer);
   
         mainBox.appendChild(singleQuestion);
-
-
     
     }
-
-
     document.body.appendChild(mainBox);
 }
-
-
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -84,9 +79,7 @@ function shuffleArray(array) {
     }
     return array;
 }
-
 function clearObjects(){
-
     const mainBox = document.querySelector('.mainBox')
     if(mainBox == null){
         return
@@ -120,30 +113,20 @@ function clearObjects(){
         input.remove();
     })
 }
-
-
 function createAnswer(text, isCorrect) {
     const answerDiv = document.createElement("div");
     answerDiv.className = "odpowiedz";
-
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.dataset.correct = isCorrect;
-
     const span = document.createElement("span");
     span.innerText = text;
-
     answerDiv.appendChild(checkbox);
     answerDiv.appendChild(span);
-
     return answerDiv;
 }
-
-
-
 function checkQuestions(){
     let totalCorrectAnswers = 0
-
     const ans = document.querySelectorAll(".questions");
     ans.forEach(function(question){
         let correctForAnswer = 0;
@@ -156,12 +139,9 @@ function checkQuestions(){
             if(input.dataset.correct == "1"){
                 correctForAnswer++;
             }
-
             if(input.checked && input.dataset.correct == "1"){
                 checkedCorrectAnswers++;
             }
-
-
             if(input.checked){
                 if(input.dataset.correct == "1"){
                     span.style.color = "#03A062"
@@ -174,16 +154,11 @@ function checkQuestions(){
                     span.style.color = "purple"
                 }
             }
-
         })
         if(checkedCorrectAnswers == correctForAnswer && correctForAnswer != 0){
             totalCorrectAnswers++
         }
 
-
- })
-
-    alert("LICZBA POPRAWNYCH ODPOWIEDZI: "+totalCorrectAnswers+"/"+questionsAmount+"("+((totalCorrectAnswers*100/questionsAmount).toFixed(1))+"%)");
+    })
     alert("LICZBA POPRAWNYCH ODPOWIEDZI: "+totalCorrectAnswers+"/"+questionsAmount+"("+(totalCorrectAnswers*100/questionsAmount)+"%)")
 }
-
